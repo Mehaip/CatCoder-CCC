803bfc31edefe879bd884b2925c3483e01f39e16
Mihai Buga <mihai.buga.bv@gmail.com>
2025-10-22 23:12:59 +0300

    new problem

---
+++ auction_bidding/bidding.py
@@ -0,0 +1,20 @@
+class Auction:
+
+    def __init__(self, current_price, bidder_input, bid_input):
+        self.biddings_input = bidder_input
+        self.current_price = current_price
+
+    bidder = []
+    bidder_maximum_bid = []
+    highest_bidder = ""
+
+
+
+
+def __main__():
+    bidding_input = input()
+
+
+
+
+
